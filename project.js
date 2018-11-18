31127bca271cab0824ce043efef8b2eb760c6ed6
Robert Newton 4 <rob_bie62@icloud.com>
2018-11-18 17:57:15 -0500

    adding to github

---
+++ project.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+
+	$('#one').click(function(){
+			$('#one').toggleClass('green-bg');
+
+	});
+
+	$('#two').click(function(){
+			$('#two').toggleClass('green-bg');
+	
+	});
+
+	$('#three').click(function(){
+			$('#three').toggleClass('red-bg');
+	
+	});
+
+	$('#four').click(function(){
+			$('#four').toggleClass('green-bg');
+	
+	});
+
+	$('#five').click(function(){
+			$('#five').toggleClass('green-bg');
+	
+	});
+
+	$('#six').click(function(){
+			$('#six').toggleClass('red-bg');
+	
+	});
+
+	$('#seven').click(function(){
+			$('#seven').toggleClass('green-bg');
+	
+	});
+
+	$('#eight').click(function(){
+			$('#eight').toggleClass('green-bg');
+	
+	});
+
+	$('#nine').click(function(){
+			$('#nine').toggleClass('red-bg');
+	
+	});
+
+	$('#ten').click(function(){
+			$('#ten').toggleClass('green-bg');
+	
+	});
+
+	$('#eleven').click(function(){
+			$('#eleven').toggleClass('green-bg');
+	
+	});
+
+	$('#twelve').click(function(){
+			$('#twelve').toggleClass('red-bg');
+	
+	});
+
+
+
+
+
+
+
+
+});
